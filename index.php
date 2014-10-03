58b95fe260ad12d84976f702282123df74531d51
Kemble Hildreth <kemble_hildreth@bitbucket.org>
2014-10-03 09:31:10 -0400

    Added some more demo items
    Will add chart demos next

--- index.php
+++ index.php
@@ -19,7 +19,35 @@
 						backgroundColor : "f1f1f1",
 						regionStyle : {
 											initial : {fill:"#a0a0a0"}
-										}
+										},
+										markers: [
+      {latLng: [41.90, 12.45], name: 'Vatican City'},
+      {latLng: [43.73, 7.41], name: 'Monaco'},
+      {latLng: [-0.52, 166.93], name: 'Nauru'},
+      {latLng: [-8.51, 179.21], name: 'Tuvalu'},
+      {latLng: [43.93, 12.46], name: 'San Marino'},
+      {latLng: [47.14, 9.52], name: 'Liechtenstein'},
+      {latLng: [7.11, 171.06], name: 'Marshall Islands'},
+      {latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis'},
+      {latLng: [3.2, 73.22], name: 'Maldives'},
+      {latLng: [35.88, 14.5], name: 'Malta'},
+      {latLng: [12.05, -61.75], name: 'Grenada'},
+      {latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines'},
+      {latLng: [13.16, -59.55], name: 'Barbados'},
+      {latLng: [17.11, -61.85], name: 'Antigua and Barbuda'},
+      {latLng: [-4.61, 55.45], name: 'Seychelles'},
+      {latLng: [7.35, 134.46], name: 'Palau'},
+      {latLng: [42.5, 1.51], name: 'Andorra'},
+      {latLng: [14.01, -60.98], name: 'Saint Lucia'},
+      {latLng: [6.91, 158.18], name: 'Federated States of Micronesia'},
+      {latLng: [1.3, 103.8], name: 'Singapore'},
+      {latLng: [1.46, 173.03], name: 'Kiribati'},
+      {latLng: [-21.13, -175.2], name: 'Tonga'},
+      {latLng: [15.3, -61.38], name: 'Dominica'},
+      {latLng: [-20.2, 57.5], name: 'Mauritius'},
+      {latLng: [26.02, 50.55], name: 'Bahrain'},
+      {latLng: [0.33, 6.73], name: 'São Tomé and Príncipe'}
+    ]
 				  
 				  });
 				});
@@ -66,7 +94,7 @@
             </p>
             <ul class="nav">
               <li class="active"><a href="#">Infoviz</a></li>
-              <li><a href="#about">Help</a></li>
+              <li><a href="#about" data-toggle="modal" data-target="#myModal" >Help</a></li>
             </ul>
           </div><!--/.nav-collapse -->
         </div>
@@ -83,7 +111,7 @@
 					<li class="active"><a data-toggle="tab" href="#sectionA">Line Graph</a></li>
 					<li><a data-toggle="tab" href="#sectionB">Scatterplot</a></li>
 					<li class="dropdown">
-						<a data-toggle="dropdown" class="dropdown-toggle" href="#">Comparisons <b class="caret"></b></a>
+						<a data-toggle="dropdown" class="dropdown-toggle" href="#">Country Comparisons <b class="caret"></b></a>
 						<ul class="dropdown-menu">
 							<li><a data-toggle="tab" href="#dropdown1">Pie Chart</a></li>
 							<li><a data-toggle="tab" href="#dropdown2">Line Chart</a></li>
@@ -110,13 +138,27 @@
 				</div>
 			</div>
 		
-       	   		 
+		  <div class="well">
+     
+			 <h4>Info for Current Country:</h4>
+		     <p>....</p>
+		      
+		 </div>       	   		
+
+
+				
 		 <div class="well">
      
 			 <h4>Guide to Using this Infoviz:</h4>
 				<p>Scroll around the map and select the countries you're interested in.  Use the time slider to see changes in world health over time.</p>
 		      
 		 </div>
+		 
+		 
+
+		 
+		 
+		 
 	   
         </div><!--/span-->
         <div class="span6">
@@ -127,14 +169,26 @@
             
 			<div class="btn-group" data-toggle="buttons">
 			  <label class="btn btn-primary active">
-				<input type="radio" name="options" id="option1" checked> Option 1 (preselected)
+				<input type="radio" name="options" id="option1" checked> Rural
 			  </label>
 			  <label class="btn btn-primary">
-				<input type="radio" name="options" id="option2"> Option 2
+				<input type="radio" name="options" id="option2"> Urban
 			  </label>
 			  <label class="btn btn-primary">
-				<input type="radio" name="options" id="option3"> Option 3
+				<input type="radio" name="options" id="option3"> Both
 			  </label>
+			</div>
+			
+			<div class="dropdown">
+			  <a data-toggle="dropdown" href="#">View Year</a>
+			  <ul class="dropdown-menu" role="menu" class="list-group">
+				<li  class="list-group-item">1990</li>
+				<li class="list-group-item">1995</li>
+				<li class="list-group-item">2000</li>
+				<li class="list-group-item">2005</li>
+				<li class="list-group-item">2010</li>
+				<li class="list-group-item">2012</li>
+			  </ul>
 			</div>
 			
             </div>
@@ -151,7 +205,16 @@
 		 		
           <div class="well">
 			<h4>Time Slider:</h4>
-			    <input type="range" name="points" min="0" max="10" style="width: 100%" onChange="alert(this.value)">
+			    <input type="range" name="points" min="0" max="6" style="width: 100%" onChange="alert(this.value)">
+  
+			
+				<div class="span2">1990</div>
+				<div class="span2">1995</div>
+				<div class="span2">2000</div>
+				<div class="span2">2005</div>
+				<div class="span2">2010</div>
+				<div class="span1">2012</div>
+
   
 		 </div>
 		 
@@ -170,6 +233,37 @@
     </div><!--/.fluid-container-->
   
 
+  
+  
+
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Need some help with our Infoviz? </h4>
+      </div>
+      <div class="modal-body">
+      
+	  
+	  <ul class="list-group">
+	  <li class="list-group-item">Cras justo odio</li>
+	  <li class="list-group-item">Dapibus ac facilisis in</li>
+	  <li class="list-group-item">Morbi leo risus</li>
+	  <li class="list-group-item">Porta ac consectetur ac</li>
+	  <li class="list-group-item">Vestibulum at eros</li>
+	</ul>
+	  
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+  
 
   </body>
 </html>
